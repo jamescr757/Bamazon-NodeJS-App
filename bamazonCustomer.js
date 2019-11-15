19f10835fb2647fbbb73012bfae3066d1b5fd0fc
jamescr757 <55260759+jamescr757@users.noreply.github.com>
2019-11-15 17:22:07 -0600

    finished part 1. mostly works. could update the layout and refactor the callback functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,13 +6,13 @@ const mysqlPassword = process.env.mysql_password;
 
 // table-cli plug-in
 const Table = require('cli-table');
- 
-// instantiate
 const table = new Table({
-    head: [chalk.green('Item ID'), chalk.green('Product Name'), chalk.green('Department'), chalk.green('Price'), chalk.green('Quantity')]
-  , colWidths: [10, 30, 20, 10, 10]
+    head: [chalk.green('Item ID'), chalk.green('Product Name'), chalk.green('Price')]
+  , colWidths: [10, 30, 10]
 });
 
+// global tax variable 
+const tax = 1.0825;
 
 // connect to database bamazon
 // create a connection 
@@ -27,31 +27,106 @@ const connection = mysql.createConnection({
 connection.connect(error => {
     if (error) throw error;
 
-    renderTable();
-    connection.end();
+    renderTableAndQuestions();
+    // connection.end();
 });
 
 // render all products when run app
 // need to select all from Table 
 // format, style, and display the info 
-function renderTable() {
+function renderTableAndQuestions() {
     connection.query(
         "SELECT * FROM products",
         (error, response) => {
             if (error) throw error;
 
             // response is an array of objects
-            // for each element need to display all keys
-            // item_id, product_name, department_name, price, and stock_quantity
+            // for each element need to display keys relevant to customer
+            // item_id, product_name, and price
             response.forEach(element => {
-                table.push([element.item_id, element.product_name, element.department_name, element.price, element.stock_quantity]);
-                // console.log(`${element.item_id} ${element.product_name} ${element.department_name} ${element.price} ${element.stock_quantity}`);
+                table.push([element.item_id, element.product_name, element.price]);
             });
             // console.log(response);
             console.log(table.toString());
+            customerPurchaseQuestions();
         }
     )
 }
 
+// ask customer two questions at load 
+// what product they would like to buy - need to input a number 
+// how much they would like to buy - need to input a number 
+function customerPurchaseQuestions() {
+    inquirer.prompt([
+        {
+            name: "userId",
+            message: "What product would you like to buy? (please input item ID #)",
+        }, 
 
+        {
+            name: "userQuantity",
+            message: "How much would you like to buy? (please input a number)"
+        }
+    ])
+    .then(answer => {
+        // need to validate user input
+        // if input NaN for either question need to ask them again
+        if (!parseInt(answer.userId) || !parseInt(answer.userQuantity)) {
+            console.log(chalk.yellow("\nPlease input a number\n"));
+            customerPurchaseQuestions();
+        }
 
+        // need to first check database quantity of item customer wants to buy 
+        checkDatabaseQuantity(answer.userId, answer.userQuantity);
+        
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+
+// grab info from database column by product id 
+// if not enough quantity, alert the customer and prompt them again
+// else, update the database and show the customer total cost of purchase
+// check price info and multiply by userQuantity - display
+function checkDatabaseQuantity(itemId, userAmount) {
+    connection.query(
+        "SELECT * FROM products WHERE ?",
+        {
+            item_id: itemId
+        },
+        function(error, response) {
+            if (error) throw error;
+
+            const databaseQuantity = response[0].stock_quantity;
+            const itemPrice = response[0].price;
+
+            if (userAmount > databaseQuantity) {
+                console.log(chalk.yellow("\nInsufficient quantity. Please purchase less of that product\n"));
+                customerPurchaseQuestions();
+            } else {
+                console.log(chalk.green("\nThanks for your business! Here's your receipt...\n"));
+                const totalPrice = (itemPrice * userAmount * tax).toFixed(2);
+                console.log("Total purchase price: " + chalk.green("$" + totalPrice));
+                console.log("");
+
+                const newDatabaseQuantity = databaseQuantity - userAmount;
+                const query = connection.query(
+                    "UPDATE products SET stock_quantity = ? WHERE item_id = ?",
+                    [
+                        newDatabaseQuantity,
+                        itemId   
+                    ],
+                    function(error, response) {
+                        if (error) console.log(error);
+                        
+                        console.log("\nstore stock updated\n");
+                        customerPurchaseQuestions();
+                    }
+                    )
+
+                // process.exit();
+            }
+        }
+    );
+}
